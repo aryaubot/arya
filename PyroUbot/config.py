119a6ba125915755a4856cec7ab427b05ddbae24
userbor13 <owneruserbot@gmail.com>
2025-07-27 21:22:34 +0700

    Add files via upload

---
+++ PyroUbot/config.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+MAX_BOT = int(os.getenv("MAX_BOT", "200"))
+
+DEVS = list(map(int, os.getenv("DEVS", "5654480935").split()))
+
+API_ID = int(os.getenv("API_ID", "20227346"))
+
+API_HASH = os.getenv("API_HASH", "0600f98a6c4721aeed6677480298ad88")
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7882864130:AAHVAPFZziqj7Fy9g9mJ4aPoSz5aJOKc2XY")
+
+OWNER_ID = int(os.getenv("OWNER_ID", "5654480935"))
+
+BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002312835928").split()))
+
+RMBG_API = os.getenv("RMBG_API", "a6qxsmMJ3CsNo7HyxuKGsP1o")
+
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://aortulsk:KxCX5EQdssavL4dm@cluster0.qsywpr2.mongodb.net/")
+
+LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-4912035642"))
