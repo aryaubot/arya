99c99ce561be2871e61d2cc81c84481b831275c7
aryaubot <aryaselibeww@gmail.com>
2025-08-02 15:20:17 +0800

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -5,7 +5,7 @@ load_dotenv(".env")
 
 MAX_BOT = int(os.getenv("MAX_BOT", "200"))
 
-DEVS = list(map(int, os.getenv("DEVS", "7524271223").split()))
+DEVS = list(map(int, os.getenv("DEVS", "7908003544").split()))
 
 API_ID = int(os.getenv("API_ID", "29839059"))
 
